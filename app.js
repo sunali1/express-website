fdfbf421107348a2fde22a5aab197ecbdebc7e14
sunaliseth <sunaliseth01@gmail.com>
2017-11-30 12:31:23 +0000

    About page added

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var nodemailer = require('nodemailer');
 
-
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -18,23 +17,11 @@ app.get('/', function(req, res){
 });
 
 app.get('/about', function(req, res){
-  res.render('index');
+  res.render('about');
 });
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://mm_recruitment_user_readonly:rebelMutualWhistle@ds037551.mongolab.com:37551/mm-recruitment';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  db.close();
+app.get('/contact', function(req, res){
+  res.render('contact');
 });
-
 app.listen(3000);
 console.log('Server is running on port 3000');
-
-module.exports = app
--- spec/testingCardSpec.js
+++ spec/testingCardSpec.js
@@ -1,9 +1,10 @@
 const request = require('supertest');
 const app = require('../app.js');
+const assert = require('assert');
 
 describe ("HelloWorld",  function() {
 
-  it ('expects page to return with code 200', function(done) {
+  it('expects page to return with code 200', function(done) {
     request(app)
      .get('/MSFT')
      .expect(200)
@@ -11,6 +12,19 @@ describe ("HelloWorld",  function() {
        if(err) return done(err);
        done();
      });
-  });
+   });
+  it("expects the page to contain some data", function(done) {
+    request(app)
+    .get('/MSFT')
+    .expect(200)
+    .expect(function(res) {
+      assert.equal(res.text, "Microsoft recently announced");
+      console.log('1')
+    .end(function(err, res){
+      if(err) return done(err);
+      done();
 
+    });
+  });
+ });
 });
