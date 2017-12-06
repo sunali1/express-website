f248eccb959657f7180e1aa36574a65528139b78
sunaliseth <sunaliseth01@gmail.com>
2017-12-06 10:44:57 +0000

    Tests to check can connect to home page

--- app.js
+++ app.js
@@ -23,6 +23,37 @@ app.get('/about', function(req, res){
 app.get('/contact', function(req, res){
   res.render('contact');
 });
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
+});
+app.post('/contact', function(req, res){
+  console.log('TEST');
+  res.send('POST Test');
+  console.log('TEST');
+  // var transporter = nodemailer.createTransport({
+  //   service: 'Gmail',
+  //   auth: {
+  //     user: 'mopmophop@gmail.com',
+  //     pass: 'samutamu'
+  //   }
+  // });
+  // var mailOptions = {
+  //   from: 'Sunali <mopmophop@gmail.com>',
+  //   to: 'sunali@aol.com',
+  //   subject: 'Website submission',
+  //   text: 'You have a submission with following details...Name: '+req.body.name+ 'Email:' +req.body.email+ 'Message: '+res.body.message,
+  //   html: '<p>You have a submission with following details...</p>Name: '+req.body.name+ 'Email:' +req.body.email+ 'Message: '+res.body.message
+  // };
+  // transporter.sendMail(mailOptions, function(error, info){
+  //   if(error){
+  //     console.log(error);
+  //     res.redirect('/');
+  //   } else {
+  //     console.log('Message sent: '+info.response);
+  //     res.redirect('/');
+  //   }
+  // });
+});
 app.listen(3000);
 console.log('Server is running on port 3000');
-// module exports = app;
+module.exports = app;
--- spec/testingCardSpec.js
+++ spec/testingCardSpec.js
@@ -6,7 +6,7 @@ describe ("HelloWorld",  function() {
 
   it('expects page to return with code 200', function(done) {
     request(app)
-     .get('/MSFT')
+     .get('/')
      .expect(200)
      .end(function(err, res){
        if(err) return done(err);
@@ -15,16 +15,25 @@ describe ("HelloWorld",  function() {
    });
   it("expects the page to contain some data", function(done) {
     request(app)
-    .get('/MSFT')
-    .expect(200)
-    .expect(function(res) {
-      assert.equal(res.text, "Microsoft recently announced");
-      console.log('1')
+    .get('/contact')
     .end(function(err, res){
       if(err) return done(err);
+    expect(res.text).toContain('Enter Name');
+      })
       done();
-
     });
   });
- });
-});
+  // it("expects to send/post data to the page ", function(done){
+  //   request(app)
+  //    .post('/')
+  //    .send({ name: 'Manny', species: 'cat' })
+  //    .set('X-API-Key', 'foobar')
+  //    .set('Accept', 'application/json')
+  //    .end(function(err, res){
+  //      if (err || !res.ok) {
+  //        alert('Oh no! error');
+  //      } else {
+  //        alert('yay got ' + JSON.stringify(res.body));
+  //      }
+  //    });
+  //  });
